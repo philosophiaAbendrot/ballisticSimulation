82d43674094e0f2cebc572b7ec964a46cd21727d
Tony Ha <tonytaesung.ha@gmail.com>
2018-01-16 12:29:15 -0500

    Further modularized application scripts. Created functions updatePanel and calculatePhysics

--- scripts/script.js
+++ scripts/script.js
@@ -14,26 +14,39 @@
 
       setInterval(onTimerTick,frameIntervalM); //33 milliseconds = ~ 30 frames per second
 
+      $('button#startCommand').on('click',function(){
+        //have to make the simulation start when the start button is pressed.
+      });
+
       function onTimerTick(){
         if (bottom <= 0 && yVel < 0){
           return;
         }
-        index += 1;
+
+        updatePanel();
+        calculatePhysics();
+
+
+        bottom = Math.round(yPos / pixelRatio);
+        left = Math.round(xPos / pixelRatio);
+        redraw(left,bottom);
+      }
+
+      function updatePanel(){
         $('li:nth-child(1)').text("Frame Number: " + index);
         $('li:nth-child(2)').text("T: : " + (index*frameInterval).toPrecision(3) + " s");
         $('li:nth-child(3)').text("Height: " + yPos.toPrecision(3) + " m");
         $('li:nth-child(4)').text("Down Range Distance: " + xPos.toPrecision(3) + " m");
         $('li:nth-child(5)').text("Horizontal Velocity: " + xVel.toPrecision(3) + " m/s");
         $('li:nth-child(6)').text("Vertical Velocity: " + yVel.toPrecision(3) + " m/s");
+      }
 
+      function calculatePhysics(){
+        index += 1;
         xPos += xVel * frameInterval;
         yPos += yVel * frameInterval;
         yVel += yAcc * frameInterval;
         xVel += xAcc * frameInterval;
-
-        bottom = Math.round(yPos / pixelRatio);
-        left = Math.round(xPos / pixelRatio);
-        redraw(left,bottom);
       }
 
       function redraw(x,y){
