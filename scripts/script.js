e86f56b6c71b9508e5ec5e92cc9bb364993543e3
Tony Ha <tonytaesung.ha@gmail.com>
2018-01-17 00:01:19 -0500

    rearranged variables

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,15 @@
     $(document).ready(function(){
-      var index = 0;
-      var xVel = 10;
-      var yVel = 20;
-      var xPos = 0;
-      var yPos = 0;
-      var yAcc = -9.8;
-      var xAcc = 0;
-      var bottom;
-      var left;
+
       var frameInterval = 0.033; //30 fps
       var frameIntervalM = frameInterval * 1000;
       var pixelRatio = 0.2; //1px = 0.2m
       var start = 0;
       var myVar;
+      var index = 0;
+      var xVel = 10;
+      var yVel = 20;
+      var xPos = 0;
+      var yPos = 0;
 
       $('button#startCommand').on('click', startFunction);
       $('button#stopCommand').on('click', stopFunction);
@@ -26,17 +23,30 @@
       }
 
       function main(){
-        if (bottom <= 0 && yVel < 0){
+        var yAcc = -9.8;
+        var xAcc = 0;
+        var bottom;
+        var left;
+
+        if (yPos < 0 && yVel < 0){
           return;
         }
 
-        updatePanel();
-        calculatePhysics();
+        index += 1;
+        xPos += xVel * frameInterval;
+        yPos += yVel * frameInterval;
+        yVel += yAcc * frameInterval;
+        xVel += xAcc * frameInterval;
+        bottom = Math.round(yPos / pixelRatio);
+        left = Math.round(xPos / pixelRatio);
+
+        updatePanel(frameInterval,xPos,yPos,xVel,yVel);
         redraw(left,bottom);
+
       }
 
 
-      function updatePanel(){
+      function updatePanel(frameInterval,xPos,yPos,xVel,yVel){
         $('li:nth-child(1)').text("Frame Number: " + index);
         $('li:nth-child(2)').text("T: : " + (index*frameInterval).toPrecision(3) + " s");
         $('li:nth-child(3)').text("Height: " + yPos.toPrecision(3) + " m");
@@ -45,17 +55,7 @@
         $('li:nth-child(6)').text("Vertical Velocity: " + yVel.toPrecision(3) + " m/s");
       }
 
-      function calculatePhysics(){
-        index += 1;
-        xPos += xVel * frameInterval;
-        yPos += yVel * frameInterval;
-        yVel += yAcc * frameInterval;
-        xVel += xAcc * frameInterval;
-      }
-
       function redraw(x,y){
-        bottom = Math.round(yPos / pixelRatio);
-        left = Math.round(xPos / pixelRatio);
         $('.sprite').css({'left': x, 'bottom':y});
       }
     });
